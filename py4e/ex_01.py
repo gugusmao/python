8b783b8e2e0a1fc26a52bbcf06a1ce30ba2a690a
gustavoagjsmais <gustavalveguma@outlook.com>
2021-10-01 22:50:26 -0300

    Exercise 0 to 3 done.

---
+++ py4e/ex_01.py
@@ -0,0 +1,11 @@
+#This script how many hours and rate you pass to calculate the pay you get
+hours = float(
+        input('Enter Hours: ')
+    )
+rate = float(
+        input('Enter Rate: ')
+    )
+
+pay = hours*rate
+print('Pay: ', pay)
+
--- py4e/ex_02.py
+++ py4e/ex_02.py
@@ -1,3 +1,19 @@
-#input that ask the name and print it out saying Hello
-name = input('Enter your name: ')
-print('Hello ', name)
+"""This script how many hours and rate you pass to calculate the pay you 
+get and if you worked above 40 hours then you get 1,5 times the houry rate."""
+
+hours = float(
+        input('Enter Hours: ')
+    )
+rate = float(
+        input('Enter Rate: ')
+    )
+pay = hours*rate
+
+#check and calculate 1.5 times the pay if worked hours is above 40
+if (hours > 40):
+    #print("Overtime")
+    pay = pay + ((hours-40) * (rate*0.5))
+#else:
+    #print("Regular")    
+
+print('Pay: ', pay)
